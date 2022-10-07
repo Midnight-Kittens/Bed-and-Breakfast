07cafacf462a32254bc0467fda2bec2cbf71f3a6
Alex <alex@bonsaiweb.dev>
2022-10-06 21:12:07 -0700

    initial commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/Midnight-Kittens/bookings/pkg/config"
+	"github.com/Midnight-Kittens/bookings/pkg/handlers"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	//"github.com/go-chi/chi/v5/middleware"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	// creat a multiplexer (http handler)
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
